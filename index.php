1b27adabdf34c1657e6b041be0132f29640e4c8c
RumiAust <rumi019aust@gmail.com>
2020-11-17 10:31:11 +0600

    logo navbar fixed

--- index.php
+++ index.php
@@ -54,8 +54,8 @@
 <div class="row">
 <div class="col-lg-12">
 <nav class="navbar navbar-expand-lg">
-<a class="navbar-brand" href="https://preview.uideck.com/items/eventify/index.html">
-<img src="./documents/logo.png" alt="Logo">
+<a >
+<img src="./documents/logo.png" width=100 alt="Logo">
 </a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTwo" aria-controls="navbarTwo" aria-expanded="false" aria-label="Toggle navigation">
 <span class="toggler-icon"></span>
